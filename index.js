c04fe37500d2b3a9fec25dac0de409b75c31f262
Ali Raza <alirazaasif2511@gmail.com>
2024-10-13 22:21:19 +0600

    updated

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
+
 dotenv.config();
 
 mongoose
@@ -17,22 +18,24 @@ mongoose
     console.log(err);
   });
 
-
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
-app.use(cors())
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use(cors());
+
+// Use PORT from environment variables, default to 3000 if not provided
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-
+// Global error handling middleware
 app.use((err, req, res, next) => {
   console.log(err);
   const statusCode = err.statusCode || 500;
