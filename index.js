79cc2dc4f13af295e206d1f763665101ce2f2513
Ali Raza <alirazaasif2511@gmail.com>
2024-10-16 13:42:24 +0600

    updated

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials:true
 }));
 
 // Use PORT from environment variables, default to 3000 if not provided
